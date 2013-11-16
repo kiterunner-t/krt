c5ea48567e2d1e6a6979577acc665c08a4f76562
krt <kiterunner_t@hotmail.com>
2013-11-16 21:20:23 +0800

    add net_misc.h

---
+++ t/linux/include/net_misc.h
@@ -0,0 +1,15 @@
+/**
+ * Copyleft (C) KRT, 2013 by kiterunner_t
+ */
+
+#ifndef NET_MISC_H_
+#define NET_MISC_H_
+
+
+#define KENDIAN_BIG       0
+#define KENDIAN_LITTLE    1
+#define KENDIAN_ERROR    -1
+
+
+#endif
+
