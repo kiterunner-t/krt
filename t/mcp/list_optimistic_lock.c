01ea1e866456d88a44d7b15d92ed8d4b83671eb0
krt <kiterunner_t@hotmail.com>
2013-11-29 18:36:48 +0800

    add list_optimistic_lock

--- t/mcp/list_optimistic_lock.c
+++ t/mcp/list_optimistic_lock.c
@@ -2,111 +2,10 @@
  * Copyleft (C) KRT, 2013 by kiterunner_t
  */
 
-#include <stdlib.h>
-#include "kmisc.h"
-#include "kitem.h"
-#include "lock.h"
-#include "thread.h"
-#include "list.h"
+#include "list.c"
 
 
-typedef struct list_node_s list_node_t;
-
-
-struct list_node_s {
-  kitem_t      item;
-  lock_t      *lock;
-  list_node_t *next;
-};
-
-
-struct list_s {
-  kitem_op_t  *op;
-  list_node_t *head;
-  list_node_t *dummy;
-};
-
-
-static void _list_free_node(list_t *l, list_node_t *node);
-static list_node_t *_list_new_node(kitem_t item, list_node_t *next);
 static kbool_t _list_validate(list_t *l, list_node_t *prev, list_node_t *cur);
-
-
-list_t *
-list_new(kitem_op_t *op)
-{
-  list_t *l;
-
-  l = (list_t *) malloc(sizeof(*l));
-  if (l == NULL)
-    return NULL;
-
-  l->dummy = _list_new_node(KITEM_NULL, NULL);
-  if (l->dummy == NULL) {
-    free(l);
-    return NULL;
-  }
-
-  l->head = _list_new_node(KITEM_NULL, l->dummy);
-  if (l->head == NULL) {
-    free(l);
-    _list_free_node(l, l->dummy);
-    return NULL;
-  }
-
-  l->op = op;
-  return l;
-}
-
-
-void
-list_destroy(list_t *l)
-{
-  list_node_t *node;
-
-  assert(l != NULL);
-
-  while (l->head != NULL) {
-    node = l->head;
-    l->head = node->next;
-    _list_free_node(l, node);
-  }
-
-  free(l);
-}
-
-
-static void
-_list_free_node(list_t *l, list_node_t *node)
-{
-  lock_destroy(node->lock);
-  node->lock = NULL;
-  node->next = NULL;
-  if (l->op->free != NULL)
-    l->op->free(node->item);
-  free(node);
-}
-
-
-static list_node_t *
-_list_new_node(kitem_t item, list_node_t *next)
-{
-  list_node_t *node;
-
-  node = (list_node_t *) malloc(sizeof(*node));
-  if (node == NULL)
-    return NULL;
-
-  node->lock = lock_new();
-  if (node->lock == NULL) {
-    free(node);
-    return NULL;
-  }
-
-  node->next = next;
-  node->item = item;
-  return node;
-}
 
 
 kerrno_t
@@ -132,8 +31,6 @@ list_insert(list_t *l, thread_t *thread, kitem_t item)
     lock(cur->lock, thread);
     if (_list_validate(l, prev, cur) == KTRUE) {
       if (n == 0) {
-        unlock(cur->lock, thread);
-        unlock(prev->lock, thread);
         ret = KEEXIST;
 
       } else {
@@ -161,7 +58,6 @@ list_delete(list_t *l, thread_t *thread, kitem_t item)
   list_node_t  *dummy = l->dummy;
   list_node_t  *prev;
   list_node_t  *cur;
-  kerrno_t      ret = KSUCCESS;
   long          n;
   kitem_cmp_pt  cmp = l->op->cmp;
 
@@ -178,15 +74,15 @@ list_delete(list_t *l, thread_t *thread, kitem_t item)
     if (_list_validate(l, prev, cur) == KTRUE) {
       if (n == 0) {
         prev->next = cur->next;
-        _list_free_node(l, cur);
-
-      } else {
-        ret = KENOTFOUND;
+        _list_free_node(l, thread, cur);
+        unlock(cur->lock, thread);
+        unlock(prev->lock, thread);
+        return KSUCCESS;
       }
 
       unlock(cur->lock, thread);
       unlock(prev->lock, thread);
-      return ret;
+      return KENOTFOUND;
     }
 
     unlock(cur->lock, thread);
