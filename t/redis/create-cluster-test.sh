0c863b7593cc6042045382c18f73c40c028ed412
krt <kiterunner_t@hotmail.com>
2014-07-15 19:52:53 +0800

    add some comments

--- t/redis/create-cluster-test.sh
+++ t/redis/create-cluster-test.sh
@@ -6,6 +6,8 @@
 # pkill redis-server
 # ps -fC redis-server
 
+# ruby redis-trib.rb create --replicas 1 127.0.0.1:7001 127.0.0.1:7003 127.0.0.1:7005 127.0.0.1:7002 127.0.0.1:7004 127.0.0.1:7006
+
 
 _redis_version=$(redis-server -v 2>/dev/null)
 _ret=$?
